331f0611d2a02e16f59ab84502be23796c94e385
stutitrip0412 <tripathistuti0412@gmail.com>
2026-06-28 13:15:39 +0530

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB", error);
+  });
 
 /*import express from "express";
 
