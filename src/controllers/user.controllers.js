a586138c0aee7137b05d709e692f7dc299af5c0f
stutitrip0412 <tripathistuti0412@gmail.com>
2026-07-01 23:54:54 +0530

    modewls and controllers updated

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,18 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async(req,res) => {
+    //get user details
+    //validation of details - not empty
+    //check if user already exists: username and email
+    //check for images, check for avataar
+    //upload them to cloudinary,avataar
+    //create user object - create entry in db
+    //remove password and refresh token field
+    //check for user creation
+    //return res 
+
+    
+    
+})
+
+export {registerUser,}
